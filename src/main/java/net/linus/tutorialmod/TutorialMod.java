900acdd8a1c6800ad04b22f80ae6eea19d59b73f
Linus Gao <linusgao123@gmail.com>
2026-04-29 23:04:58 -0400

    added new items of garnet and raw garnet

--- src/main/java/net/linus/tutorialmod/TutorialMod.java
+++ src/main/java/net/linus/tutorialmod/TutorialMod.java
@@ -2,11 +2,12 @@ package net.linus.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.linus.tutorialmod.items.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
-	public static final String MOD_ID = "tutorial-mod";
+	public static final String MOD_ID = "tutorialmod";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -19,6 +20,6 @@ public class TutorialMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
